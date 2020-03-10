e771b731b2de82cefb25c18195950dfdceb9bcca
Alexander Supe <alex.m.supe@gmail.com>
2020-03-10 11:19:30 +0100

    Initial Setup

---
+++ ImageFilters/ImageFilters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilters
+//
+//  Created by Alexander Supe on 10.03.20.
+//  Copyright © 2020 Alexander Supe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
